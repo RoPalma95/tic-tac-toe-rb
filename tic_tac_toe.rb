6c86599917ddeb06fb7b9ec45c7cc93262182b04
RoPalma95 <r.palma995@gmail.com>
2020-11-04 16:15:20 -0600

    Checks horizontal & vertical win

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,6 +7,8 @@ class Match
   public
   attr_reader :player1, :player2
 
+  @@winner = false
+
   def initialize
     print "Player 1>> "
     @player1 = Player.new(1)
@@ -14,10 +16,13 @@ class Match
     @player2 = Player.new(2)
     puts "\n"
 
-    @row1 = [1, 2, 3]
-    @row2 = [4, 5, 6]
-    @row3 = [7, 8, 9]
-    @board = [@row1, @row2, @row3]
+    @board = [[1, 2, "X"], [4, 5, "X"], [7, 8, "X"]]
+    # @board = Array.new(3) { Array.new(3) {0}}
+    # for i in 0..2
+    #   for j in 0..2
+    #     @board[j][i] = 3 * j + i + 1
+    #   end
+    # end
   end
 
   def print_board
@@ -46,13 +51,16 @@ class Match
     end
   end
 
-  def update_board(player_number)
+  def select_player(player_number)
     if player_number == 1
       player = player1
     else
       player = player2
     end
+  end
 
+  def update_board(player_number)
+    player = select_player(player_number)
     move = player.make_move.to_i
 
     if is_move_available?(move)
@@ -68,6 +76,45 @@ class Match
     else
       puts "\n\tCell is unavailable. Pick a different one."
     end
+  end
+
+  def check_rows(player_number, cols = @board)
+    player = select_player(player_number)
+    cols.reduce(false) do |win, row|
+      unless win == true
+        row.all?(player.sign)
+      else
+        win
+      end
+      # binding.pry
+    end
+  end
+
+  def check_columns(player_number)
+    flipped_board = Array.new(3) { Array.new(3) {0}}
+    for i in 0..2
+      for j in 0..2
+        flipped_board[i][j] = board[j][i] 
+      end
+    end
+    # binding.pry
+    check_rows(player_number, flipped_board)
+  end
+
+  def check_diagonals(player_number)
+    player = select_player(player_number)
+    win = true
+    for i in 0..2
+      if board[i][i] != player.sign || board[2 - i][2 - 1] != player.sign
+        win = false
+      end
+    end
+    win
+    # binding.pry
+  end
+
+  def winner(player_number)
+    check_rows(player_number) || check_columns(player_number) || check_diagonals(player_number)
   end
 
   def reset_board
@@ -100,10 +147,10 @@ round = Match.new
 winner = false
 puts "Xs: #{round.player1.name}\tOs: #{round.player2.name}"
 round.print_board
-
-until winner
-  round.update_board(1)
-  round.print_board
-  round.update_board(2)
-  round.print_board
-end
+puts round.winner(1)
+# until winner
+#   round.update_board(1)
+#   round.print_board
+#   round.update_board(2)
+#   round.print_board
+# end
